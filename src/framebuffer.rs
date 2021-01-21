82b781618ebbfa7a0ea2bb3549fc1f037a5a2f5c
Safin Singh <safin.singh@gmail.com>
2021-01-21 11:25:01 -0800

    const fns on framebuffer

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -13,39 +13,39 @@ pub struct FramebufferTag {
 
 impl FramebufferTag {
     /// Get the start address of the framebuffer
-    pub fn start_address(&self) -> usize {
+    pub const fn start_address(&self) -> usize {
         self.address as usize
     }
 
     /// Get the end address of the framebuffer
     /// 
     /// Identical to `framebuffer_info.start_address() + framebuffer_info.size()`
-    pub fn end_address(&self) -> usize {
+    pub const fn end_address(&self) -> usize {
         self.address as usize + self.size()
     }
 
     /// Get the size of the framebuffer
-    pub fn size(&self) -> usize {
+    pub const fn size(&self) -> usize {
         self.pitch as usize * self.height as usize * (self.bpp as usize / 8)
     }
 
     /// Get the width of the framebuffer in pixels
-    pub fn width(&self) -> u16 {
+    pub const fn width(&self) -> u16 {
         self.width
     }
 
     /// Get the height of the framebuffer in pixels
-    pub fn height(&self) -> u16 {
+    pub const fn height(&self) -> u16 {
         self.height
     }
 
     /// Get the bytes per line of the framebuffer
-    pub fn pitch(&self) -> u16 {
+    pub const fn pitch(&self) -> u16 {
         self.pitch
     }
 
     /// Get the bits per pixel of the framebuffer
-    pub fn bpp(&self) -> u16 {
+    pub const fn bpp(&self) -> u16 {
         self.bpp
     }
 }
